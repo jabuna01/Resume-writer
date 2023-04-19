86ec3883476998b5332b7bed2e73ad84512cfe3f
jabuna01 <jabuna@fusemachines.com>
2023-04-19 16:48:57 +0545

    merge master

---
+++ src/scenes/ResumeUpload.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+const ResumeUpload = () => {
+  return (
+    <div>
+      <Row>
+        <Col>test</Col>
+        <Col>test</Col>
+        <Col>test</Col>
+      </Row>
+    </div>
+  );
+};
+
+export default ResumeUpload;
