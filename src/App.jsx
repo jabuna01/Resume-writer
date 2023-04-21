29107fba98fdaf53e5a42628f544e36c57b5ea13
ShrabinFuse11 <shrabin@fusemachines.com>
2023-04-21 10:44:04 +0545

    design fixes on login page and upload page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { authenticate } from "./services/authentication.service";
 import { router } from "./routes";
-import './sass/style.scss';
+import "./sass/style.scss";
 
 const filterPublicRoutes = (isPublic) => {
   const routes = router.filter((route) => route.public === isPublic);
@@ -13,11 +13,11 @@ function App() {
     <>
       {authenticate() ? (
         <RouterProvider
-          router={createBrowserRouter(filterPublicRoutes(false))}
+          router={createBrowserRouter(filterPublicRoutes(true))}
         />
       ) : (
         <RouterProvider
-          router={createBrowserRouter(filterPublicRoutes(true))}
+          router={createBrowserRouter(filterPublicRoutes(false))}
         />
       )}
     </>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import LoginPage from "../components/pages/LoginPage";
 import LandingPage from "../components/pages/LandingPage";
-
+import Upload from "../scenes/upload";
 export const router = [
   {
     path: "/",
@@ -10,6 +10,12 @@ export const router = [
   {
     path: "/landing-screen",
     element: <LandingPage />,
+    public: true,
+  },
+  //Design Templates
+  {
+    path: "/Design/upload",
+    element: <Upload />,
     public: true,
   },
 ];
