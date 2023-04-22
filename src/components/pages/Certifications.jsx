c1e8e94f4fe94d2ab258eb9f297919a874775bf5
prashantch265 <prashantchy265@gmail.com>
2023-04-22 12:41:07 +0545

    feat(certifications)

--- src/components/pages/Certifications.jsx
+++ src/components/pages/Certifications.jsx
@@ -8,10 +8,12 @@ import {
   ButtonToolbar,
 } from "react-bootstrap";
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function CertificationPage() {
-  const [form, addForm] = useState([]);
-  const [certification, setCertification] = useState([]);
+  const form = useSelector(
+    (state) => state.apiResponse.response.certifications
+  );
 
   const handleAddCertification = (e) => {
     const element = form.length + 1;
@@ -40,10 +42,10 @@ export default function CertificationPage() {
               Save and Continue
             </Button>
           </div>
-          <Button variant="info" onClick={handleAddCertification}>
+          {/* <Button variant="info" onClick={handleAddCertification}>
             + Add Certifications
-          </Button>
-          {form.map((index) => (
+          </Button> */}
+          {form.map((data, index) => (
             <Form className="form-wrapper" key={index}>
               <Row>
                 <Col md={12}>
@@ -54,13 +56,19 @@ export default function CertificationPage() {
                     <Form.Label>Certificate Title</Form.Label>
                     <Form.Control
                       type="text"
-                      name = "certification_name"
+                      name="certification_name"
                       placeholder="Enter Certificate Title"
+                      value={data.certification_name}
                     />
                   </Form.Group>
                   <Form.Group className="mb-3">
                     <Form.Label>Certificate Course Link</Form.Label>
-                    <Form.Control type="text" name = "certification_link" placeholder="Enter Certificate link" />
+                    <Form.Control
+                      type="text"
+                      name="certification_link"
+                      placeholder="Enter Certificate link"
+                      value={data.certification_link}
+                    />
                   </Form.Group>
                 </Col>
                 <Col md={12}>
@@ -71,23 +79,23 @@ export default function CertificationPage() {
                     <Form.Label>Certification Description</Form.Label>
                     <Form.Control
                       as="textarea"
-                      name = "certification_description"
+                      name="certification_description"
                       placeholder="Enter your Certificate Description"
+                      value={data.certification_description}
                     />
                   </Form.Group>
                 </Col>
-                
               </Row>
               <div className="tab-title-block">
                 <div></div>
-                <ButtonToolbar>
+                {/* <ButtonToolbar>
                   <ButtonGroup className="m-2">
                     <Button variant="secondary"> Add</Button>
                   </ButtonGroup>
                   <ButtonGroup className="m-2">
                     <Button variant="secondary"> Remove</Button>
                   </ButtonGroup>
-                </ButtonToolbar>
+                </ButtonToolbar> */}
               </div>
             </Form>
           ))}
